fb85f1d701533cd6394d7b85770fe879de5b9de8
Ann-VZ <annzoykina@gmail.com>
2021-10-27 14:47:09 +0500

    Portrait of a woman 700:560

--- src/main_package/ApplicationFrame.java
+++ src/main_package/ApplicationFrame.java
@@ -3,19 +3,25 @@ package main_package;
 import javax.swing.*;
 import java.awt.*;
 
-public class ApplicationFrame {
+public class ApplicationFrame extends JLabel {
+    /*@Override
+    protected void paintComponent(Graphics g) {
+        mainLabel.setSize(frame.getSize());
+        super.paintComponent(g);
+    }*/
+
     private JFrame frame;
-    private ImageIcon portraitIcon;
+    private ImageIcon background;
     private JLabel mainLabel;
     ApplicationFrame() {
-        portraitIcon = new ImageIcon(this.getClass().
-                getResource("/Kramskoy_Portrait_of_a_Woman_Bakcgraound_Picture.jpg"));
+        background = new ImageIcon(this.getClass().
+                getResource("/Background3.jpg"));
 
-        mainLabel = new JLabel(portraitIcon);
+        mainLabel = new JLabel(background);
         mainLabel.setSize(300, 200);
 
         frame = new JFrame("Schedule analization");
-        frame.setBounds(10, 20, 700, 600);
+        frame.setBounds(10, 20, 700, 560);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setLayout(new BorderLayout());
