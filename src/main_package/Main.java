e66406a047222c91f31199697d7713c63eb66e42
Ann-VZ <annzoykina@gmail.com>
2021-10-27 14:54:04 +0500

    Just frame

--- src/main_package/Main.java
+++ src/main_package/Main.java
@@ -2,6 +2,6 @@ package main_package;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationFrame frame = new ApplicationFrame();
+        new ApplicationFrame();
     }
 }
