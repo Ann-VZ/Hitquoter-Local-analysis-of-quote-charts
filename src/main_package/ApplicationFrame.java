87663615604c80c4f0e2e7a0721ff796780a35c5
Ann-VZ <annzoykina@gmail.com>
2021-11-01 17:33:12 +0500

    textarea

--- src/main_package/ApplicationFrame.java
+++ src/main_package/ApplicationFrame.java
@@ -1,6 +1,7 @@
 package main_package;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,13 +18,14 @@ public class ApplicationFrame extends JLabel {
         JLabel greetingLabel = new JLabel("<html><body style='text-align: center'>Program for estimating the independence of" +
                 "<br>local fluctuations in stock quotes, version 1.0");
         greetingLabel.setHorizontalAlignment(JLabel.CENTER);
-        greetingLabel.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+        greetingLabel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
         greetingLabel.setFont(new Font("Monotype Corsiva", Font.ITALIC, 35));
         greetingLabel.setForeground(new Color(10, 63, 222));
 
-        JTextArea output = new JTextArea("Results", 5, 30);
+        JTextArea output = new JTextArea("Results", 5, 8);
         //output.setTabSize(3);
         //output.setMaximumSize(new Dimension(50, 50));
+        output.setBorder(new EmptyBorder(5, 10, 10, 10));
         output.setLineWrap(true);
         output.setEditable(false);
         output.setFont(new Font("Verdana", Font.PLAIN, 18));
@@ -44,7 +46,7 @@ public class ApplicationFrame extends JLabel {
             public void actionPerformed(ActionEvent e) {
                 ScheduleAnalizer analizer = new ScheduleAnalizer();
                 if (!analizer.checkSchedule()) {
-                    output.append("The user hasn't chosen a rectange!\n");
+                    output.append("The user hasn't chosen a rectangle!\n");
                 } else {
                     output.append(analizer.getClosest()+"\n");
                 }
@@ -52,7 +54,10 @@ public class ApplicationFrame extends JLabel {
         });
 
         Container container = frame.getContentPane();
-        container.setLayout(new GridLayout(3, 1));
+        //frame.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
+        GridLayout mainLayout = new GridLayout(3, 1);
+        mainLayout.setVgap(5);
+        container.setLayout(mainLayout);
         container.add(greetingLabel);//, BorderLayout.NORTH);
 
         JPanel mainPanel = new JPanel();
@@ -70,21 +75,32 @@ public class ApplicationFrame extends JLabel {
         JLabel textLabel = new JLabel("Results");
         //textLabel.setPreferredSize(new Dimension(100, 50));
         textLabel.setFont(new Font("Monotype Corsiva", Font.ITALIC, 25));
+        textLabel.setBorder(new EmptyBorder(30, 30, 30, 30));
         textLabel.setForeground(new Color(10, 63, 222));
         //container.add(textLabel);
 
         JScrollPane resultsScroll = new JScrollPane(output);
         //resultsScroll.setPreferredSize(new Dimension(500, 500));
         //container.add(resultsScroll);
+        frame.setVisible(true);
+
+        JFrame f = new JFrame("Just frame");
+        f.setBounds(20, 20, 600, 600);
+        f.setLayout(new FlowLayout());
 
         JPanel textPanel = new JPanel();
-        textLabel.setLayout(new BorderLayout());
-        //textPanel.setLayout(new FlowLayout());
-        textPanel.add(textLabel, BorderLayout.NORTH);
-        textPanel.add(resultsScroll, BorderLayout.SOUTH);
+        //textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+        textPanel.setLayout(new VerticalLayout());
+        textPanel.add(textLabel);
+        textPanel.add(resultsScroll);
+        textPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+        frame.add(textPanel);
 
-        container.add(textPanel);
+        /*f.add(textPanel);
+        f.setVisible(true);*/
 
-        frame.setVisible(true);
+        //container.add(textPanel);
+
+        //frame.setVisible(true);
     }
 }
